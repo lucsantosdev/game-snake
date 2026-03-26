56bbe7d5f55e1147e363eb7335c7a3f7fee912be
Lucas Santos <lucsantosdev@gmail.com>
2026-03-26 20:04:15 -0300

    feat: create snake draw

--- snake.py
+++ snake.py
@@ -8,6 +8,11 @@ CELL_SIZE = 25
 WINDOW_WIDTH = CELL_SIZE * COLS
 WINDOW_HEIGHT = CELL_SIZE * ROWS
 
+class Cell:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 # Game Window
 window = tkinter.Tk()
 window.title("Snake Game by @lucsantosdev")
@@ -28,5 +33,14 @@ x = (screen_width // 2) - (window_width // 2)
 y = (screen_height // 2) - (window_height // 2)
 window.geometry(f"{window_width}x{window_height}+{x}+{y}") # format "(w)x(h)+(x)+(y)" 
 
+# Initialize the Game
+snake = Cell(5*CELL_SIZE, 5*CELL_SIZE) # Starting position of the snake's head on the single cell grid
 
+
+def draw():
+    global snake
+    canvas.create_rectangle(snake.x, snake.y, snake.x + CELL_SIZE, snake.y + CELL_SIZE, fill="lime green")
+    window.after(100, draw)  # Schedule the draw function to be called every 100 milliseconds
+
+draw()  # Start the drawing loop
 window.mainloop()
